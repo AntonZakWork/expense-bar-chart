034837e7ce60e389d4b5c9374b4cb0770b67e179
AntonZakWork <antonzakrevskiy@gmail.com>
2022-06-16 21:54:07 +0300

    FE mentor challenge

--- deploy/src/App.js
+++ deploy/src/App.js
@@ -17,49 +17,47 @@ const App = () => {
   return (
     <>
       <div className="app">
-        <div className="background">
-          <div className="chartContainer">
-            <div className="chartHeader">
-              <div className="headerInfo">
-                My balance
-                <h3>$921.48</h3>
-              </div>
-              <div className="headerIcon">
-                <svg width="72" height="48" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" fillRule="evenodd">
-                    <circle fill="#382314" cx="48" cy="24" r="24" />
-                    <circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23" />
-                  </g>
-                </svg>
+        <div className="chartContainer">
+          <div className="chartHeader">
+            <div className="headerInfo">
+              My balance
+              <h3>$921.48</h3>
+            </div>
+            <div className="headerIcon">
+              <svg width="72" height="48" viewBox="0 0 72 48" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fillRule="evenodd">
+                  <circle fill="#382314" cx="48" cy="24" r="24" />
+                  <circle stroke="#FFF" strokeWidth="2" cx="24" cy="24" r="23" />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="chartContent">
+            <div className="barChartContainer">
+              <div className="barChartHeader">Spending - Last 7 days</div>
+              <div className="barChart">
+                {data.map((el) => (
+                  <>
+                    <div className="day" key={el.day}>
+                      <div
+                        className="bar"
+                        style={heightCalculator(el)}
+                        data-content={`$${el.amount}`}></div>
+                      <div>{el.day}</div>
+                    </div>
+                  </>
+                ))}
               </div>
             </div>
-            <div className="chartContent">
-              <div className="barChartContainer">
-                <div className="barChartHeader">Spending - Last 7 days</div>
-                <div className="barChart">
-                  {data.map((el) => (
-                    <>
-                      <div className="day" key={el.day}>
-                        <div
-                          className="bar"
-                          style={heightCalculator(el)}
-                          data-content={`$${el.amount}`}></div>
-                        <div>{el.day}</div>
-                      </div>
-                    </>
-                  ))}
-                </div>
+            <div className="chartInfo">
+              <div className="monthSpending">
+                <div>Total this month</div>
+                <h3>$478.33</h3>
               </div>
-              <div className="chartInfo">
-                <div className="monthSpending">
-                  <div>Total this month</div>
-                  <h3>$478.33</h3>
-                </div>
-                <div className="monthPercent">
-                  <div>
-                    <b>+2.4%</b>
-                    <div> from last month</div>
-                  </div>
+              <div className="monthPercent">
+                <div>
+                  <b>+2.4%</b>
+                  <div> from last month</div>
                 </div>
               </div>
             </div>
